19324c788aae9fbe090dee7cba7cb29a49b45ef5
Vikms <vms1995@hotmail.com>
2022-01-29 21:02:57 +0100

    Done second tutorial

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import _ from 'lodash';
 import myName from './myName';
-import './style.css';
-import image from './site-logo.1fcab817090e78435061.svg'
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
+    const btn = document.createElement('button');
+    
     element.textContent = myName('Cody');
     element.classList.add('hello');
+    
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
 
-    // Add image to our existing div
-    const myIcon = new Image();
-    myIcon.src = image;
-    element.appendChild(myIcon);
-
+    element.appendChild(btn);
     return element;
   }
   
